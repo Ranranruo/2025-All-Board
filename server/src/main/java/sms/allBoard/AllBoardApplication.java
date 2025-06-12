16a5007c7c77c6bc27e8ba87273b97b29ff93b04
Ranranruo <tlsalstjr58@gmail.com>
2025-06-12 14:07:28 +0900

    create project

---
+++ server/src/main/java/sms/allBoard/AllBoardApplication.java
@@ -0,0 +1,13 @@
+package sms.allBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllBoardApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/sms/allBoard/AllBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package sms.allBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
