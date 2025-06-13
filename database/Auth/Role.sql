24339576b7e2356b2fddc5c916c7b84bbed1f838
Ranranruo <tlsalstjr58@gmail.com>
2025-06-13 16:42:46 +0900

    feat: auth
    회원 관련 테이블 기본 구조는 다 완성한것같다.
    
    Resolve

--- database/Auth/Role.sql
+++ database/Auth/Role.sql
@@ -3,9 +3,9 @@ CREATE TABLE Role (
   id          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '고유번호',
   name        VARCHAR(30)     NOT NULL                COMMENT '이름',
   description TEXT            COMMENT '설명',
-  create_at   DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
-  update_at   DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간',
-  flag        BOOLEAN         NOT NULL DEFAULT FALSE COMMENT '플래그',
+  created_at   DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
+  updated_at   DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간',
+  deleted_at   DATETIME        COMMENT '삭제 시간',
   PRIMARY KEY (id)
 ) COMMENT = '권한';
 
