1a16282f7c0629460a7b17e464f5db2b6639e99f
minseok shin <tlsalstjr58@gmail.com>
2025-07-01 16:10:33 +0900

    feat: role entity
    role entity 작성
    
    Resolve

--- server/role-service/src/main/java/com/allboard/role/Controller/RoleController.java
+++ server/role-service/src/main/java/com/allboard/role/Controller/RoleController.java
@@ -1,4 +1,9 @@
 package com.allboard.role.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/role")
 public class RoleController {
 }
--- server/role-service/src/main/java/com/allboard/role/Model/MemberRole.java
+++ server/role-service/src/main/java/com/allboard/role/Model/MemberRole.java
@@ -1,4 +1,17 @@
 package com.allboard.role.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class MemberRole {
+    @Id
+    private Long id;
+
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "role_id")
+    private Long roleId;
 }
