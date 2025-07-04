0f7556915b58f984d724f7ef8ec1fac217704d12
minseok shin <tlsalstjr58@gmail.com>
2025-07-04 16:44:36 +0900

    rename: member_role, role
    member, member_role, role
    auth로 병합
    
    Resolve

--- database/Auth/Init01.sql
+++ database/Auth/Init01.sql
@@ -4,15 +4,28 @@ SET character_set_connection = utf8mb4;
 SET character_set_client = utf8mb4;
 SET character_set_results = utf8mb4;
 
-DROP DATABASE IF EXISTS role;
+DROP DATABASE IF EXISTS auth;
 
-CREATE DATABASE role;
-USE Role;
+CREATE DATABASE auth;
+USE auth;
 
 DROP TABLE IF EXISTS member_role;
+DROP TABLE IF EXISTS member;
 DROP TABLE IF EXISTS role;
 
 -- CREATE
+CREATE TABLE member (
+  id           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '고유번호',
+  username     VARCHAR(20)     NOT NULL UNIQUE COMMENT '이름',
+  email        VARCHAR(50)     NOT NULL COMMENT '이메일',
+  display_name VARCHAR(12)     NOT NULL DEFAULT '유저' COMMENT '닉네임',
+  password     VARCHAR(100)    NOT NULL COMMENT '비밀번호',
+  created_at   DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
+  updated_at   DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시간',
+  deleted_at   DATETIME        COMMENT '삭제 시간',
+  PRIMARY KEY (id)
+) COMMENT = '회원';
+
 
 CREATE TABLE role (
   id          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '고유번호',
@@ -37,4 +50,3 @@ CREATE TABLE member_role (
 
 INSERT INTO role (name, description) VALUES ('ADMIN', '관리자');
 INSERT INTO role (name, description) VALUES ('MEMBER', '회원');
-
